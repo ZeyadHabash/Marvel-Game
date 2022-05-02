c18ff1c249aee84400da51c026ae2e39f7720974
fatty <zeyad.habashy02@gmail.com>
2022-05-02 21:22:10 +0200

    removed nina comment :3

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -230,4 +230,3 @@ public class Game {
 		return BOARDWIDTH;
 	}
 }
-//heyooooo~~
--- src/model/effects/Effect.java
+++ src/model/effects/Effect.java
@@ -14,7 +14,6 @@ public abstract class Effect implements Cloneable {
 	}
 
 	public void apply(Champion c){
-
 	}
 	public void remove(Champion c){
 	}
