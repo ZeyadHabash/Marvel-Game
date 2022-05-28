dc1fb03bb4900dd5a99d1a229603c723f690404a
fatty <zeyad.habashy02@gmail.com>
2022-05-28 13:49:48 +0200

    we now load the champions and abilities before displaying the window, doesn't open if the csv files containing the data do not exist.

--- src/views/StartScreen.java
+++ src/views/StartScreen.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import views.TeamSelect;
 import engine.Player;
 import engine.Game;
+
 import static engine.Game.loadChampions;
 import static engine.Game.loadAbilities;
 
@@ -35,19 +36,25 @@ public class StartScreen extends Application implements EventHandler<ActionEvent
 
     public Stage mainWindow;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        mainWindow = primaryStage;
-        primaryStage.setTitle("marvel");
-        primaryStage.setScene(scene(width = 900, height = 600));
-        primaryStage.show();
+        try {
+            loadChampions("Champions.csv");
+            loadAbilities("Abilities.csv");
+            mainWindow = primaryStage;
+            primaryStage.setTitle("marvel");
+            primaryStage.setScene(scene(width = 900, height = 600));
+            primaryStage.show();
+        } catch (IOException e) {
+            AlertBox.display(e.getLocalizedMessage(), "Please make sure the file is in the game directory and try running the game again");
+        }
     }
 
-    public Scene scene(double width, double height){
+    public Scene scene(double width, double height) {
 
         enterP1Name = new Label("Enter Player 1 name");
         player1Name = new TextField();
@@ -66,15 +73,9 @@ public class StartScreen extends Application implements EventHandler<ActionEvent
     @Override
     public void handle(ActionEvent event) {
         if (event.getSource() == confirm) {
-            try {
-                System.out.println("loading game");
-                newGame = new Game(new Player(player1Name.getText()), new Player(player2Name.getText()));
-                loadChampions("Champions.csv");
-                loadAbilities("Abilities.csv");
-                mainWindow.setScene(TeamSelect.scene(width,height));
-            } catch (IOException e) {
-                AlertBox.display(e.getLocalizedMessage(), "Please make sure the file is in the game directory and try again");
-            }
+            System.out.println("starting game");
+            newGame = new Game(new Player(player1Name.getText()), new Player(player2Name.getText()));
+            mainWindow.setScene(TeamSelect.scene(width, height));
         }
     }
 }
